6d176a10812c57a5585d6664973a9f3273c9f874
Yash Lekhwani <147407640+shylekhwani@users.noreply.github.com>
2023-12-19 21:56:48 +0530

    Add files via upload

---
+++ fundamentals3.js
@@ -0,0 +1,14 @@
+// Ans 5 :
+let principal = 5000;
+let rate = 5;
+let timeperiod = 3;
+
+const calcsimpleinterest = (principal * rate * timeperiod) / 100;
+console.log(`result = ${calcsimpleinterest}`);
+
+//Ans 6:
+let weight = 72; //kg
+let height = 1.7; //meters
+
+const calcbmi = (weight / height) * height;
+console.log(`result = ${calcbmi}`);
